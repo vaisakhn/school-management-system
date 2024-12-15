e42bb71f74ca0a3a2274ad1bff7550d05d4488a3
vaisakh <vaisakhn02@gmail.com>
2024-12-15 12:17:23 +0530

    staff dashoard completed

--- officestaff/urls.py
+++ officestaff/urls.py
@@ -3,4 +3,10 @@ from officestaff import views
 
 urlpatterns = [
     path('staff-login/',views.StaffLoginView.as_view(),name="staff-login"),
+    path('students/', views.StudentListAPIView.as_view(), name='student-list'), 
+    path('students/<int:pk>/',views.StudentRetrieveAPIView.as_view(), name='student-detail'),
+    path('feeshistory/',views.FeesHistoryListCreateAPIView.as_view(), name='feeshistory-list-create'), 
+    path('feeshistory/<int:pk>/',views.FeesHistoryRetrieveUpdateDestroyAPIView.as_view(), name='feeshistory-detail-update-delete'),
+    path('libraryhistory/',views.LibraryHistoryListAPIView.as_view(), name='libraryhistory-list'),
+    path('libraryhistory/<int:pk>/',views.LibraryHistoryRetrieveAPIView.as_view(), name='libraryhistory-detail'),
 ]
--- officestaff/views.py
+++ officestaff/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from Admin.serializers import StaffLoginSerializer
+from Admin.serializers import StaffLoginSerializer,StudentSerializer,LibraryHistorySerializer,FeesHistorySerializer
 from rest_framework import generics,status
 from rest_framework.response import Response
-from Admin.models import User
+from Admin.models import User,Student,LibraryHistory,FeesHistory
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import BasePermission
 
 # Create your views here.
+
+# creating custom  permision for office staff
+class IsOfficeStaff(BasePermission):
+    def has_permission(self, request, view): # Check if the user is authenticated and is a admin
+        return request.user and (request.user.role=="office_staff")
 
 #login function for staff
 class StaffLoginView(generics.GenericAPIView):
@@ -34,3 +40,47 @@ class StaffLoginView(generics.GenericAPIView):
             }, status=status.HTTP_200_OK)
         else:
             return Response({'detail': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+
+# take list of students
+class StudentListAPIView(generics.ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes=[IsOfficeStaff]
+
+# get student details
+class StudentRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes=[IsOfficeStaff]
+
+
+# creating fees history
+class FeesHistoryListCreateAPIView(generics.ListCreateAPIView):
+    queryset = FeesHistory.objects.all()
+    serializer_class = FeesHistorySerializer
+    permission_classes=[IsOfficeStaff]
+
+# fee history get,update,delete view
+class FeesHistoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = FeesHistory.objects.all()
+    serializer_class = FeesHistorySerializer
+    permission_classes=[IsOfficeStaff]
+    def delete(self, request, *args, **kwargs): 
+        fees_history = self.get_object() 
+        fees_history.delete() 
+        return Response({"message": "fees history deleted successfully!"}, status=status.HTTP_204_NO_CONTENT)
+    
+
+# view library history list
+class LibraryHistoryListAPIView(generics.ListAPIView):
+    queryset = LibraryHistory.objects.all()
+    serializer_class = LibraryHistorySerializer
+    permission_classes=[IsOfficeStaff]
+
+
+# view library history details for students
+class LibraryHistoryRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = LibraryHistory.objects.all()
+    serializer_class = LibraryHistorySerializer
+    permission_classes=[IsOfficeStaff]
